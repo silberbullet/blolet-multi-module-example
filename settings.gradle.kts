320f63a9960e34ae8273c30214a5a426dad1877b
silberbullet <pkw961028@naver.com>
2025-03-15 19:01:00 +0900

    build(blolet): refactor settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,37 @@
 rootProject.name = "blolet"
 
+val core = file("core").walkTopDown().maxDepth(3).filter(File::isDirectory).associateBy(File::getName)
+val board = file("services/board").walkTopDown().maxDepth(3).filter(File::isDirectory).associateBy(File::getName)
+
 include(
     "monolithic"
     ,"common"
-    ,"nettee-exception-handler-core"
+)
+
+// CORE
+include(
+    "nettee-exception-handler-core"
     ,"nettee-jpa-core"
 )
 
-val coreDirs = file("core").walkTopDown().filter(File::isDirectory).associateBy(File::getName)
-val servicesDir = file("services")
+project(":nettee-exception-handler-core").projectDir = core["nettee-exception-handler-core"]!!
+project(":nettee-jpa-core"              ).projectDir = core["nettee-jpa-core"]!!
 
-project(":nettee-exception-handler-core").projectDir = coreDirs["nettee-exception-handler-core"]!!
-project(":nettee-jpa-core").projectDir = coreDirs["nettee-jpa-core"]!!
+// SERVICE/BOARD
+include(
+    "boardApi"
+    ,"boardApi:domain"
+    ,"boardApi:exception"
+    ,"boardApi:readmodel"
+    ,"boardApplication"
+    ,"boardDrivenRdbJpa"
+    ,"boardDrivingWeb"
+)
+
+project(":boardApi"          ).projectDir = board["api"]!!
+project(":boardApi:domain"   ).projectDir = board["domain"]!!
+project(":boardApi:exception").projectDir = board["exception"]!!
+project(":boardApi:readmodel").projectDir = board["readmodel"]!!
+project(":boardApplication"  ).projectDir = board["application"]!!
+project(":boardDrivenRdbJpa" ).projectDir = board["rdb"]!!
+project(":boardDrivingWeb"   ).projectDir = board["rest"]!!
