942536791cd29270146af7e94984c9491e8ac607
silberbullet <pkw961028@naver.com>
2025-03-16 23:55:01 +0900

    feat(monolithic): add component scan to scan board service bean

--- monolithic/client/src/main/java/me/nettee/client/ClientApplication.java
+++ monolithic/client/src/main/java/me/nettee/client/ClientApplication.java
@@ -2,8 +2,15 @@ package me.nettee.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+       "config","api","service",
+        "mapper","entity","repository","handler"
+})
+@EnableJpaRepositories(basePackages = "repository") // 리포지토리 패키지 지정
 public class ClientApplication {
     
     public static void main(String[] args) {
