739be1c30e4bdab6011ca184c60d727bb3d6eb2d
silberbullet <pkw961028@naver.com>
2025-03-16 23:51:40 +0900

    build(board): add database and flyway to postgresql

--- services/board/driven/rdb/postgresql/build.gradle.kts
+++ services/board/driven/rdb/postgresql/build.gradle.kts
@@ -1,8 +1,12 @@
+plugins {
+    `java-library`
+}
+
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
-    implementation(project(":jpa-core"))
     implementation(project(":board-application"))
+    api(project(":jpa-core"))
 
     // spring
     implementation("org.springframework:spring-context")
@@ -11,6 +15,12 @@ dependencies {
     implementation("org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+    // database
+    runtimeOnly("org.postgresql:postgresql:42.7.4")
+
+    // flyway
+    implementation("org.flywaydb:flyway-database-postgresql")
 
     // lombok
     compileOnly("org.projectlombok:lombok")
@@ -21,4 +31,13 @@ dependencies {
     annotationProcessor("com.querydsl:querydsl-apt:${dependencyManagement.importedProperties["querydsl.version"]}:jakarta")
     annotationProcessor("jakarta.persistence:jakarta.persistence-api")
     annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+}
+
+tasks.processResources {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE // 또는 DuplicatesStrategy.WARN 사용 가능
+
+    from("src/main/resources") {
+        include("db-local.yml") // 로컬 환경 설정
+        include("db-dev.yml")   // 개발 환경 설정
+    }
 }
