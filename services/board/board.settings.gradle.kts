64de938d992bbb99b43c5c26c8cb63a202a5cf76
silberbullet <pkw961028@naver.com>
2025-03-16 23:47:12 +0900

    build(board): add board-service module

--- services/board/board.settings.gradle.kts
+++ services/board/board.settings.gradle.kts
@@ -2,7 +2,8 @@ val board = rootDir.resolve("services").resolve("board").walkTopDown().maxDepth(
 
 // SERVICE/BOARD
 include(
-    "board-api"
+     "board-service"
+    ,"board-api"
     ,"board-api:domain"
     ,"board-api:exception"
     ,"board-api:readmodel"
@@ -11,6 +12,7 @@ include(
     ,"board-rest-webmvc-adapter"
 )
 
+project(":board-service"               ).projectDir = board["board"]!!
 project(":board-api"                   ).projectDir = board["api"]!!
 project(":board-api:domain"            ).projectDir = board["domain"]!!
 project(":board-api:exception"         ).projectDir = board["exception"]!!
@@ -18,3 +20,4 @@ project(":board-api:readmodel"         ).projectDir = board["readmodel"]!!
 project(":board-application"           ).projectDir = board["application"]!!
 project(":board-rdb-postgresql-adapter").projectDir = board["postgresql"]!!
 project(":board-rest-webmvc-adapter"   ).projectDir = board["web-mvc"]!!
+
