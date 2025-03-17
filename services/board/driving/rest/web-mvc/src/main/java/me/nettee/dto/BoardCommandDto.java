716f21a7355fc2feb9ae72c3a4ec86faa660547d
silberbullet <pkw961028@naver.com>
2025-03-17 23:30:39 +0900

    feat(board): add board status field

--- services/board/driving/rest/web-mvc/src/main/java/me/nettee/dto/BoardCommandDto.java
+++ services/board/driving/rest/web-mvc/src/main/java/me/nettee/dto/BoardCommandDto.java
@@ -2,9 +2,11 @@ package me.nettee.dto;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import me.nettee.Board;
+import me.nettee.type.BoardStatus;
 
 public final class BoardCommandDto {
 
@@ -17,7 +19,9 @@ public final class BoardCommandDto {
             String title,
             @NotBlank(message = "본문을 입력하십시오")
             @Size(min = 3, message = "제목은 세 글자 이상 입력하세요.")
-            String content
+            String content,
+            @NotNull(message = "상태를 입력하십시오")
+            BoardStatus status
     ) {
     }
 
