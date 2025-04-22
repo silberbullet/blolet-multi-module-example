789dcdf6d226e978a252cb579a6ba76dc4b6c48b
silberbullet <pkw961028@naver.com>
2025-04-22 23:23:06 +0900

    build(core): fix build.gradle.kts yml

--- core/nettee-exception-handler-core/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-version = "0.0.1-SNAPSHOT"
-
-dependencies {
-    implementation(project(":common"))
-
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-}
--- core/nettee-jpa-core/build.gradle.kts
+++ core/nettee-jpa-core/build.gradle.kts
@@ -1,15 +1,7 @@
-plugins {
-    `java-library`
-}
-
 version = "0.0.1-SNAPSHOT"
 
 dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
-
-    // lombok
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
 
     // querydsl
     // JPA 엔티티를 분석하고 Q클래스 자동생성
