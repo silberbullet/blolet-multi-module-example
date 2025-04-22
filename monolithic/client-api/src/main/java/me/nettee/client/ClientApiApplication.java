fe313ab68f796273ba05572e9a23fc2d53cd33c4
silberbullet <pkw961028@naver.com>
2025-04-22 23:33:59 +0900

    feat(monolith): add configure scan annotation in main application

--- monolithic/client-api/src/main/java/me/nettee/client/ClientApiApplication.java
+++ monolithic/client-api/src/main/java/me/nettee/client/ClientApiApplication.java
@@ -2,8 +2,10 @@ package me.nettee.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(scanBasePackages = "me.nettee")
+@ConfigurationPropertiesScan(basePackages = "me.nettee")
 public class ClientApiApplication {
     
     public static void main(String[] args) {
