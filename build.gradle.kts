14d378199b42092028d92755ec17c0569cf91c3c
silberbullet <pkw961028@naver.com>
2025-04-22 23:22:36 +0900

    build(blolet): fix build.gradle.kts yml to add kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,56 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    id("org.springframework.boot") version "3.4.3" apply false
-    id("io.spring.dependency-management") version "1.1.7" apply false
+    id("java")
+    id("java-library")
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management")  version "1.1.7"
+    id("org.jetbrains.kotlin.jvm") version "2.1.20"
+    id("org.jetbrains.kotlin.plugin.spring") version "2.1.20"
 }
 
-subprojects{
-    apply(plugin = "java")
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "io.spring.dependency-management")
-
+allprojects {
     group = "me.nettee"
+    version = "1.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("java")
+        plugin("java-library")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+    }
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    dependencies {
+        if(project.name != "common") {
+            api(project(":common"))
+        }
+
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+        testCompileOnly("org.projectlombok:lombok")
+        testAnnotationProcessor("org.projectlombok:lombok")
+
+        compileOnly("org.springframework:spring-web")
+        compileOnly("org.springframework:spring-context")
+
+        testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+        testImplementation("io.mockk:mockk:1.13.12")
+        testImplementation(kotlin("script-runtime"))
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     }
 
     tasks.withType<BootJar>{
@@ -23,5 +60,16 @@ subprojects{
     tasks.withType<Jar>{
         enabled = true
     }
-}
 
+    tasks.test {
+        useJUnitPlatform()
+    }
+
+    kotlin{
+        sourceSets {
+            test {
+                kotlin.srcDirs(listOf("src/test/kotlin"))
+            }
+        }
+    }
+}
