bfa62ffec4c813c5e58288614c2e330c8eeb6326
silberbullet <pkw961028@naver.com>
2025-04-22 23:31:51 +0900

    build(core): fix core.settings.gradle.kts

--- core/core.settings.gradle.kts
+++ core/core.settings.gradle.kts
@@ -4,11 +4,11 @@ val core = rootDir.resolve("core").walkTopDown().maxDepth(3).filter(File::isDire
 include(
     "exception-handler-core"
     ,"jpa-core"
-    ,"nettee-cors-api"
-    ,"nettee-cors-webmvc"
+    ,"cors-api"
+    ,"cors-webmvc"
 )
 
 project(":exception-handler-core").projectDir = core["nettee-exception-handler-core"]!!
 project(":jpa-core"              ).projectDir = core["nettee-jpa-core"]!!
-project(":nettee-cors-api"       ).projectDir = core["nettee-cors-api"]!!
-project(":nettee-cors-webmvc"    ).projectDir = core["nettee-cors-webmvc"]!!
+project(":cors-api"       ).projectDir = core["nettee-cors-api"]!!
+project(":cors-webmvc"    ).projectDir = core["nettee-cors-webmvc"]!!
