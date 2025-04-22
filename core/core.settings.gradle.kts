001bc0a8ffb0bb2cc81b35e6adf836e054390a4e
silberbullet <pkw961028@naver.com>
2025-04-23 01:51:12 +0900

    feat(core): add snowflake module setting

--- core/core.settings.gradle.kts
+++ core/core.settings.gradle.kts
@@ -6,9 +6,13 @@ include(
     ,"jpa-core"
     ,"cors-api"
     ,"cors-webmvc"
+    ,"snowflake-id-api"
+    ,"snowflake-id-hibernate"
 )
 
 project(":exception-handler-core").projectDir = core["nettee-exception-handler-core"]!!
 project(":jpa-core"              ).projectDir = core["nettee-jpa-core"]!!
-project(":cors-api"       ).projectDir = core["nettee-cors-api"]!!
-project(":cors-webmvc"    ).projectDir = core["nettee-cors-webmvc"]!!
+project(":cors-api"              ).projectDir = core["nettee-cors-api"]!!
+project(":cors-webmvc"           ).projectDir = core["nettee-cors-webmvc"]!!
+project(":snowflake-id-api"      ).projectDir = core["nettee-snowflake-id-api"]!!
+project(":snowflake-id-hibernate").projectDir = core["nettee-snowflake-id-hibernate"]!!
