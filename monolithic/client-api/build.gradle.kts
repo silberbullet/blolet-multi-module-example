99ff212207b69cc7a89daa7a4609af292e233a39
silberbullet <pkw961028@naver.com>
2025-04-22 23:33:24 +0900

    build(monolith): add cors-webmvc module and test library

--- monolithic/client-api/build.gradle.kts
+++ monolithic/client-api/build.gradle.kts
@@ -2,24 +2,23 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 version = "0.0.1-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
-}
-
 dependencies {
-    // module
+    // core
     implementation(project(":exception-handler-core"))
     implementation(project(":jpa-core"))
-    implementation(project(":board-service"))
-
-    //spring
+    implementation(project(":cors-webmvc"))
+    // service
+    implementation(project(":board"))
+    // webmvc
     implementation("org.springframework.boot:spring-boot-starter-web")
+    // db
+    runtimeOnly("org.postgresql:postgresql:42.7.4")
+    // flyway
+    implementation("org.flywaydb:flyway-database-postgresql")
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
 tasks.withType<Test> {
